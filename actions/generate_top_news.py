24afa37a977f5d04aaa300c79ec1fe9642f39c44
kevincheng96 <kevincheng96@hotmail.com>
2019-12-08 18:16:21 -0800

    Use multithreading to improve latency of generate_top_news() from ~25s to 2.5s.

--- actions/generate_top_news.py
+++ actions/generate_top_news.py
@@ -2,8 +2,10 @@ import sys
 sys.path.insert(1,'/Users/Kevincheng96/Documents/Coding Projects/Python projects/robin_stocks')
 
 from actions.objects import Stock
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import robin_stocks as r
 import heapq
+import time
 
 '''
 Robinhood includes dividends as part of your net gain. This script removes
@@ -20,32 +22,35 @@ username = ''
 password = ''
 #!!!
 
+# TODO: Add async calls!!!
 def generate_top_news():
+	start_time = time.time()
 	r.login(username,password)
 
 	# Get list of stocks in watchlist and positions.
 	stocks_dict = {}
 	watchlist = r.get_watchlist_by_name()
-	for element in watchlist:
-		instrument = element['instrument']
-		stock_info = r.request_get(instrument)
-		ticker = stock_info['symbol']
-		stock = Stock(stock_info['simple_name'], ticker)
-		stocks_dict[ticker] = stock
 	positions = r.get_current_positions()
-	for position in positions:
-		instrument = position['instrument']
+	stocks = watchlist + positions
+
+	# Get stock info (e.g. ticker, name) for each stock.
+	with ThreadPoolExecutor(max_workers=20) as executor:
+		stock_info_futures = [executor.submit(r.request_get, stock['instrument']) for stock in stocks]
+	for stock_info_future in as_completed(stock_info_futures):
+		stock_info = stock_info_future.result()
 		ticker = stock_info['symbol']
 		stock = Stock(stock_info['simple_name'], ticker)
 		stocks_dict[ticker] = stock
-	for stock in stocks_dict.values():
-		print(stock)
+	print("--- %s seconds --- (Got all tickers)" % (time.time() - start_time))
 
 	# Get top news for all stocks (prioritized by num_clicks).
 	# TODO: Consider prioritizing by date also.
 	news_heap = []
-	for stock in stocks_dict.values():
-		news = r.get_news(stock.ticker)
+	news_dict = {}
+	with ThreadPoolExecutor(max_workers=20) as executor:
+		news_futures = [executor.submit(r.get_news, stock.ticker) for stock in stocks_dict.values()]
+	for news_future in as_completed(news_futures):
+		news = news_future.result()
 		if not news:
 			continue
 		recent_news = news[0]
@@ -55,6 +60,7 @@ def generate_top_news():
 		title = recent_news['title']
 		# (-views, ticker, source, title)
 		heapq.heappush(news_heap, (-num_clicks, stock.ticker, source, title))
+	print("--- %s seconds --- (Got all news)" % (time.time() - start_time))
 
 	# TTS for top news.
 	news_aggregate = []
@@ -63,4 +69,5 @@ def generate_top_news():
 		stock_news_string = "Top news for stock " + stocks_dict[ticker].name
 		news_source_string = "From source, " + source
 		news_aggregate.append(stock_news_string + "\n" + news_source_string + "\n" + title)
+	print("--- %s seconds --- (End)" % (time.time() - start_time))
 	return ''.join(news_aggregate)
